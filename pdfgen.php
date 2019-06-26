0914358ea6e760e726f2c54f40ae58d6c96e6bd2
SansonRoot <asanyuo123@gmail.com>
2019-06-26 08:46:44 +0000

    Updated print

--- pdfgen.php
+++ pdfgen.php
@@ -1,56 +1,56 @@
 <?php
-    // include('databaseconnection.php');
-    // include('session.php');
-    // require('fpdf.php');
-    // $data = $_POST["filtered"];
-    // echo json_decode($data);
+     include('databaseconnection.php');
+     include('session.php');
+     require('fpdf.php');
+     //$data = $_POST["filtered"];
+     //echo json_decode($data);
 
     $data = json_decode($_POST["filtered"]);
-    echo $data[0];
-    // class PDF extends FPDF{
+   // echo $data[0];
+    class PDF extends FPDF{
         
-    //     function header(){
-    //         include('session.php');
-    //         date_default_timezone_set('UTC');
-    //         $this->image('assets/img/logo.png',10,6);
-    //         $created_at = date("F j, Y, g:i a");
-    //         $this->SetFont('Arial','B',14);
-    //         $this->Cell(276,5,'SCB FCC INTERNAL WATCHLIST',0,0,'C');
-    //         $this->Ln();
-    //         $this->SetFont('Times','',12);
-    //         $this->Cell(276,10,$created_at,0,0,'C');
-    //         $this->Ln();
-    //         $this->SetFont('Times','',12);
-    //         $this->Cell(276,10,'printed by: '. $Employee_ID,0,0,'C');
-    //         $this->Ln(40);
-    //     }
+         function header(){
+             include('session.php');
+             date_default_timezone_set('UTC');
+             $this->image('assets/img/logo.png',10,6);
+             $created_at = date("F j, Y, g:i a");
+             $this->SetFont('Arial','B',14);
+             $this->Cell(276,5,'SCB FCC INTERNAL WATCHLIST',0,0,'C');
+             $this->Ln();
+             $this->SetFont('Times','',12);
+             $this->Cell(276,10,$created_at,0,0,'C');
+             $this->Ln();
+             $this->SetFont('Times','',12);
+             $this->Cell(276,10,'printed by: '. $Employee_ID,0,0,'C');
+             $this->Ln(40);
+         }
  
-    //     function footer(){
-    //         $this->SetY(-15);
-    //         $this->SetFont('Arial','',8);
-    //         $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-    //     }
+         function footer(){
+             $this->SetY(-15);
+             $this->SetFont('Arial','',8);
+             $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+         }
     
-    //     function headerTable(){
-    //         $this->SetFont('Times','B',12);
-    //         $this->Cell(40,10,'Date of Notice',1,0,'C');
-    //         $this->Cell(40,10,'Reference Number',1,0,'C');
-    //         $this->Cell(40,10,'Individual/Entity',1,0,'C');
-    //         $this->Cell(30,10,'LinkedTo',1,0,'C');
-    //         $this->Cell(30,10,'Subject Name',1,0,'C');
-    //         $this->Cell(30,10,'Alias',1,0,'C');
-    //         $this->Cell(22,10,'Country',1,0,'C');
-    //         $this->Cell(32,10,'Further Info',1,0,'C');
-    //         $this->Ln();
-    //     }
+         function headerTable(){
+             $this->SetFont('Times','B',12);
+             $this->Cell(40,10,'Date of Notice',1,0,'C');
+             $this->Cell(40,10,'Reference Number',1,0,'C');
+             $this->Cell(40,10,'Individual/Entity',1,0,'C');
+             $this->Cell(30,10,'LinkedTo',1,0,'C');
+             $this->Cell(30,10,'Subject Name',1,0,'C');
+             $this->Cell(30,10,'Alias',1,0,'C');
+             $this->Cell(22,10,'Country',1,0,'C');
+             $this->Cell(32,10,'Further Info',1,0,'C');
+             $this->Ln();
+         }
          
 
 
         
-        // function viewTable(){
-        //     include('databaseconnection.php');
-        //     $this->SetFont('Times','',12);
-        //     $sql_pdf = "SELECT * FROM Watchlist";
+         function viewTable(){
+             include('databaseconnection.php');
+             $this->SetFont('Times','',12);
+             $sql_pdf = "SELECT * FROM Watchlist";
         //     $result = json_decode($data);
 //             while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)){
 //                 $this->Cell(40,10,$row['DateOfNotice'],1,0,'C');
@@ -63,17 +63,28 @@
 //                 $this->Cell(32,10,$row['FurtherInfo'],1,0,'R');
 //                 $this->Ln();
 //             }
+
+              for($i=0; $i<count($data); $i++){
+                $this->Cell(40,10,$data[$i][0],1,0,'C');
+                $this->Cell(40,10,$data[$i][1],1,0,'L');
+                $this->Cell(40,10,$data[$i][2],1,0,'L');
+                $this->Cell(30,10,$data[$i][3],1,0,'L');
+                $this->Cell(30,10,$data[$i][4],1,0,'L');
+                $this->Cell(30,10,$data[$i][5],1,0,'L');
+                $this->Cell(22,10,$data[$i][6],1,0,'R');
+
+              }
             
     //         echo $result;
-    //     }
+         }
 
     // }
 
-    // $pdf = new PDF();
-    // $pdf->AliasNbPages();
-    // $pdf->AddPage('L','A4',0);
-    // $pdf->headerTable();
-    // $pdf->viewTable();
-    // $pdf->Output();
+     $pdf = new PDF();
+     $pdf->AliasNbPages();
+     $pdf->AddPage('L','A4',0);
+     $pdf->headerTable();
+     $pdf->viewTable();
+     $pdf->Output();
 
 ?>
